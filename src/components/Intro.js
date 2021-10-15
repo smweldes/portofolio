8767cfbe698564439367d7fcbbea5c6875bb5d99
smweldes <weldesiem@gmail.com>
2021-10-15 15:10:42 -0700

    added left and right

---
+++ src/components/Intro.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./main.css"
+
+function Intro() {
+    return (
+        <div className='i'>
+            <div className='i-left'>
+                <div className='i-left-wrapper'>
+                      <h2 className='i-intro'>Hi, my name is</h2>
+                      <h2 className='i-name'>John David</h2>
+                </div>
+            </div>
+            <div className='i-right'>right</div>
+        </div>
+    )
+}
+
+export default Intro
